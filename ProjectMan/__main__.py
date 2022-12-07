7e3eeddc835a08f2980639a36a68e0147197ac5e
manageya33 <119881005+manageya33@users.noreply.github.com>
2022-12-07 19:42:51 +0700

    Update __main__.py

--- ProjectMan/__main__.py
+++ ProjectMan/__main__.py
@@ -34,7 +34,7 @@ async def main():
             await bot.start()
             bot.me = await bot.get_me()
             await bot.join_chat("Jual_Nokos_Telegram")
-            await bot.join_chat("roombotmel")
+            await bot.join_chat("tempatbotpoa88")
             try:
                 await bot.send_message(
                     BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
